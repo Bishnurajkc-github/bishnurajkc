4110078399529d9c7a87b686fd4532d9779d08bd
Bishnu Raj KC <157229290+Bishnurajkc-github@users.noreply.github.com>
2026-01-29 14:54:16 +0545

    Update calculator.js
    js code added.

--- static/calculator/js/calculator.js
+++ static/calculator/js/calculator.js
@@ -1,1 +1,31 @@
+function calc(op) {
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+
+    fetch("/calculate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ op, a, b })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.error) {
+            document.getElementById("result").innerText = data.error;
+            return;
+        }
+
+        document.getElementById("result").innerText =
+            "Result: " + data.result;
+
+        const history = document.getElementById("history");
+        history.innerHTML = "";
+        data.history.forEach(item => {
+            const li = document.createElement("li");
+            li.innerText = item;
+            history.appendChild(li);
+        });
+    });
+}
 
